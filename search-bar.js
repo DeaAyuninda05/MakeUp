420e44f591911c67d4c6764d061d05c867ffd1d8
DeaAyuninda05 <126029193+DeaAyuninda05@users.noreply.github.com>
2023-05-06 14:53:40 +0700

    Update search-bar.js

--- search-bar.js
+++ search-bar.js
@@ -86,8 +86,7 @@ class SearchBar extends HTMLElement {
         </div>
       `;
    
-      this.shadowDOM.querySelector('#searchButtonElement')
-      .addEventListener('click', this._clickEvent);
+      this.shadowDOM.querySelector('#searchButtonElement').addEventListener('click', this._clickEvent);
     }
   }
    
